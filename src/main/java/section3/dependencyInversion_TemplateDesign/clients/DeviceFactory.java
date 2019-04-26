c865912c39278b993d14c775b2e02b219ed0e70d
sayaka-tamura <arccycle@gmail.com>
2019-04-26 20:43:42 +0900

    Refactored file location and make packages for the project

---
+++ src/main/java/section3/dependencyInversion_TemplateDesign/clients/DeviceFactory.java
@@ -0,0 +1,11 @@
+/**
+ * @author : Sayaka Tamura
+ * Apr 26, 2019
+ * Udemy Master Object Oriented Design in Java
+ * 
+ */
+package section3.dependencyInversion_TemplateDesign.clients;
+
+public class DeviceFactory {
+
+}
