0b748788ec5778b17ea159059c869e3585a9a2ab
sayaka-tamura <arccycle@gmail.com>
2019-04-26 21:07:35 +0900

    Made DeviceFactory.java

--- src/main/java/section3/dependencyInversion_TemplateDesign/clients/DeviceFactory.java
+++ src/main/java/section3/dependencyInversion_TemplateDesign/clients/DeviceFactory.java
@@ -2,10 +2,17 @@
  * @author : Sayaka Tamura
  * Apr 26, 2019
  * Udemy Master Object Oriented Design in Java
- * 
+ * Section 3, Lecture 20
+ * Dependency Inversion & Template Pattern
  */
 package section3.dependencyInversion_TemplateDesign.clients;
 
-public class DeviceFactory {
+import section3.dependencyInversion_TemplateDesign.processes.GeneralManufacturingProcess;
+import section3.dependencyInversion_TemplateDesign.processes.SmartphoneManufacturingProcess;
 
+public class DeviceFactory {
+	public static void main(String[] args) {
+		GeneralManufacturingProcess manufacturer = new SmartphoneManufacturingProcess("IPhone Process");
+		manufacturer.launchProcess();
+	}
 }
